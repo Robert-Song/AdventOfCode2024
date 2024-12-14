0041c782befd4a1f7910b8b0ecbbf76543b191b1
Robert-Song <thinkmisul@gmail.com>
2024-12-13 23:50:06 -0500

    day1part1 done

--- first/first.py
+++ first/first.py
@@ -5,8 +5,6 @@ def radix_sort(arr):
     max_value = max(arr)
     num_digits = len(str(max_value))
 
-    print(num_digits)
-
     # Perform counting sort for each digit, starting from the least significant digit
     for exp in range(num_digits):
         arr = counting_sort(arr, exp)
@@ -24,7 +22,7 @@ def counting_sort(arr, exp):
 
     # Count the occurrences of each digit
     for i in range(n):
-        index = (arr[i] // (10 ** exp)) % 10
+        index = (int(arr[i]) // (10 ** exp)) % 10
         count[index] += 1
 
     # Calculate the cumulative count
@@ -34,7 +32,7 @@ def counting_sort(arr, exp):
     # Build the output array
     i = n - 1
     while i >= 0:
-        index = (arr[i] // (10 ** exp)) % 10
+        index = (int(arr[i]) // (10 ** exp)) % 10
         output[count[index] - 1] = arr[i]
         count[index] -= 1
         i -= 1
@@ -45,10 +43,26 @@ if __name__ == '__main__':
 
     leftArray = []
     rightArray = []
+    count = 0;
 
     with open('input.txt', 'r') as file:
         # Read each line in the file
         for line in file:
             # Print each line
-            append()
-            print(line.strip())
+            temp = line.strip().split()
+            leftArray.append(temp[0])
+            rightArray.append(temp[1])
+            count += 1
+
+    leftArray = radix_sort(leftArray)
+    rightArray = radix_sort(rightArray)
+
+    sum = 0
+
+    for i in range(count):
+        sum += abs(int(leftArray[i]) - int(rightArray[i]))
+
+    print(sum)
+
+
+
