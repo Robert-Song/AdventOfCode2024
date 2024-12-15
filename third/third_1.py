6d95ce1e4806764658ce294cdc28d9a94f6c4342
Robert-Song <thinkmisul@gmail.com>
2024-12-15 04:19:15 -0500

    day3 part1 RegEx

---
+++ third/third_1.py
@@ -0,0 +1,14 @@
+import re
+
+if __name__ == '__main__':
+    sum = 0
+
+    with (open('input.txt', 'r') as file):
+        # Read each line in the file
+        for line in file:
+            x = re.findall("mul\([0-9]+,[0-9]+\)", line)
+            for i in x:
+                temp = i[4:-1].split(",")
+                sum += int(temp[0]) * int(temp[1])
+
+    print(sum)
