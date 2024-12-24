8b736ae83076d3165ec7b14e0cf501f5e07455d0
robert-song <song669@purdue.edu>
2024-12-23 20:15:15 -0500

    trace 2D array

--- main.py
+++ main.py
@@ -1,56 +1,59 @@
-from collections import deque
+import random
+from collections import Counter
 
-# Function to return list containing vertices in Topological order.
+# 초기화
+deck = [1, 1, 1, 2, 2, 3]  # 덱 구성
+num_simulations = 10**7     # 시뮬레이션 반복 횟수
+play_count = Counter()      # 각 카드가 플레이된 횟수 기록
+removed_cards = Counter() # 각 카드가 버려진 횟수 기록
 
+deck_state = deck.copy()
+random.shuffle(deck_state)  # 덱 섞기
+hand = [deck_state.pop(), deck_state.pop()]  # 초기 손패
+# 시뮬레이션 실행
+for _ in range(num_simulations):
+    if 3 in hand:  # 3카드가 있으면 플레이
+        play_count[3] += 1
+        hand.remove(3)
+        removed_cards[hand[0]] += 1
+        hand.clear()
+        hand.append(deck_state.pop())
+        if len(deck_state) == 0 :
+            deck_state = deck.copy()
+            random.shuffle(deck_state)
+        hand.append(deck_state.pop())
+        if len(deck_state) == 0:
+            deck_state = deck.copy()
+            random.shuffle(deck_state)
+    elif 2 in hand:  # 3카드가 없고 2카드가 있으면 플레이
+        play_count[2] += 1
+        hand.remove(2)
+        removed_cards[hand[0]] += 1
+        hand.clear()
+        hand.append(deck_state.pop())
+        if len(deck_state) == 0:
+            deck_state = deck.copy()
+            random.shuffle(deck_state)
+        hand.append(deck_state.pop())
+        if len(deck_state) == 0 :
+            deck_state = deck.copy()
+            random.shuffle(deck_state)
+    elif hand == [1, 1]:  # 1카드만 있으면 플레이
+        play_count[1] += 1
+        hand.remove(1)
+        hand.append(deck_state.pop())
+        if len(deck_state) == 0:
+            deck_state = deck.copy()
+            random.shuffle(deck_state)
+    else:
+        print("error!")
+        break
 
-def topological_sort(adj, V):
-    # Vector to store indegree of each vertex
-    indegree = [0] * V
-    for i in range(V):
-        for vertex in adj[i]:
-            indegree[vertex] += 1
+# 결과 출력
+total_plays = sum(play_count.values())
+print({k: v / total_plays for k, v in play_count.items()})
+print({k: v for k, v in removed_cards.items()})
 
-    # Queue to store vertices with indegree 0
-    q = deque()
-    for i in range(V):
-        if indegree[i] == 0:
-            q.append(i)
-    result = []
-    while q:
-        node = q.popleft()
-        result.append(node)
-        # Decrease indegree of adjacent vertices as the current node is in topological order
-        for adjacent in adj[node]:
-            indegree[adjacent] -= 1
-            # If indegree becomes 0, push it to the queue
-            if indegree[adjacent] == 0:
-                q.append(adjacent)
+print(0.4633399*4 + 0.2885781*4 + 0.248082*3)
+print(1/6*4 + 1 + 1/2*3)
 
-    # Check for cycle
-    if len(result) != V:
-        print("Graph contains cycle!")
-        return []
-    return result
-
-
-if __name__ == "__main__":
-    n = 6  # Number of nodes
-
-    # Edges
-    edges = [[0, 1], [1, 2], [2, 3], [4, 5], [5, 1], [5, 2]]
-
-    # Graph represented as an adjacency list
-    adj = [[] for _ in range(n)]
-
-    # Constructing adjacency list
-    for edge in edges:
-        adj[edge[0]].append(edge[1])
-
-    print(adj)
-    # Performing topological sort
-    print("Topological sorting of the graph:", end=" ")
-    result = topological_sort(adj, n)
-
-    # Displaying result
-    for vertex in result:
-        print(vertex, end=" ")
---
+++ sixth/sixth_1.py
@@ -0,0 +1,75 @@
+from enum import Enum
+
+from numpy.f2py.auxfuncs import throw_error
+
+
+class Direction(Enum):
+    NORTH = 0
+    EAST = 1
+    SOUTH = 2
+    WEST = 3
+
+if __name__ == '__main__':
+    count = 0
+    arr = []
+    #read file, separate characters, save like a 2D array
+    with (open('input.txt', 'r') as file):
+        for line in file:
+            arr.append(list(line[:-1]))
+            if '^' in line:
+                i = count
+                j = line.index('^')
+                arr[i][j] = 'X'
+            count += 1
+
+    count = 1
+    direction = Direction.NORTH
+    print(i, j)
+    try:
+        while True:
+            print(i, j)
+            match direction:
+                case Direction.NORTH:
+                    i_next = i - 1
+                    j_next = j
+                case Direction.EAST:
+                    i_next = i
+                    j_next = j + 1
+                case Direction.SOUTH:
+                    i_next = i + 1
+                    j_next = j
+                case Direction.WEST:
+                    i_next = i
+                    j_next = j - 1
+            if(i_next < 0 or i_next >= len(arr) or j_next < 0 or j_next >= len(arr[0])):
+                raise IndexError
+            if arr[i_next][j_next] == '.':
+                count += 1
+                arr[i_next][j_next] = 'X'
+                i = i_next
+                j = j_next
+                continue
+            elif arr[i_next][j_next] == 'X':
+                i = i_next
+                j = j_next
+                continue
+            elif arr[i_next][j_next] == '#':
+                match direction:
+                    case Direction.NORTH:
+                        direction = Direction.EAST
+                    case Direction.EAST:
+                        direction = Direction.SOUTH
+                    case Direction.SOUTH:
+                        direction = Direction.WEST
+                    case Direction.WEST:
+                        direction = Direction.NORTH
+                continue
+    except IndexError: print("reached end")
+    except: print("what?")
+    else: print("infinite loop?")
+
+    for i in range(len(arr)):
+        print(arr[i])
+
+    print(count)
+
